4a0d3e7fccc1b5582cdeba4890b20b25c6491278
Tom Dobing <tom.dobing@assurant.com>
2023-01-31 12:31:16 +0000

    #50 Class created

---
+++ Gaze.BusinessLogic.Config/Validations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Gaze.BusinessLogic.Config
+{
+    public class Validations
+    {
+
+        public static bool IsValidEmail(string email)
+        {
+            string pattern = @"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*" + "@" + @"((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$";
+            return Regex.IsMatch(email, pattern);
+        }
+
+
+
+    }
+}
